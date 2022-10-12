11256d54ae8c7baeac6a2913e7149710d28bc660
yagusti26 <yasminagusti@gmail.com>
2022-10-12 17:58:07 -0300

    Se insertó componente social

--- ProyectoIntegrador_AgustiYasmin/src/app/app.module.ts
+++ ProyectoIntegrador_AgustiYasmin/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
